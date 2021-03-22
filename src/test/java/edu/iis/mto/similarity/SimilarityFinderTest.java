1ea04eddf346c69a709b54d257c93669fccec049
monikaciezka <223959@edu.p.lodz.pl>
2021-03-22 19:14:22 +0100

    Testing the value of the intersect

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,8 +1,7 @@
 package edu.iis.mto.similarity;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 import edu.iis.mto.searcher.SearchResult;
 import edu.iis.mto.searcher.SequenceSearcher;
@@ -44,5 +43,30 @@ class SimilarityFinderTest {
 
 
     }
+
+    @Test
+    void testIntersectValue() {
+        class SequenceSearcherMock  implements SequenceSearcher {
+            SearchResult searchResult;
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+
+                if (elem % 2 == 0){
+                    return searchResult = new SearchResult(SearchResult.builder().withFound(false));
+                } else if (elem > 0 && elem < 20) {
+                    return searchResult = new SearchResult(SearchResult.builder().withFound(true));
+                }
+                return null;
+            }
+        }
+        int[] seq1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] seq2 = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        SequenceSearcherMock searcher = new SequenceSearcherMock();
+        SimilarityFinder indexJaccard = new SimilarityFinder(searcher);
+        indexJaccard.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(5, indexJaccard.getIntersectSize());
+
+    }
+
 
 }
